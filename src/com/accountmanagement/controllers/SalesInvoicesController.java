7637bca41744cb6ac72b2a8a6e02b2b37e48e87f
Eltayeb Ibrahim <tayeb1293@yahoo.com>
2024-07-08 19:01:20 +0200

    feature: sales invoices list ui

--- src/com/accountmanagement/controllers/SalesInvoicesController.java
+++ src/com/accountmanagement/controllers/SalesInvoicesController.java
@@ -1,20 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.accountmanagement.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
-/**
- * FXML Controller class
- *
- * @author asdf
- */
 public class SalesInvoicesController implements Initializable {
+
+    @FXML
+    private TextField txtCustomerName;
+    @FXML
+    private TextField txtCustomerPhone;
+    @FXML
+    private Label lbCustomerStatus;
+    @FXML
+    private TextField txtCustomerNameSearch;
+    @FXML
+    private TableView<?> tbCustomer;
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+    @FXML
+    private TableColumn<?, ?> colCustomerName;
+    @FXML
+    private TableColumn<?, ?> colCustomerPhone;
+    @FXML
+    private DatePicker txtDate;
+    @FXML
+    private ComboBox<?> cbInvoiceType;
+    @FXML
+    private TextField txtFilePath;
+    @FXML
+    private ComboBox<?> cbCustomerName;
+    @FXML
+    private TextField txtProductName;
+    @FXML
+    private TextField txtQty;
+    @FXML
+    private TextField txtPrice;
+    @FXML
+    private Label lbInvoiceTotal;
+    @FXML
+    private Label lbDiscount;
+    @FXML
+    private Label lbTax;
+    @FXML
+    private Label lbTotal;
+    @FXML
+    private TextField txtTax;
+    @FXML
+    private TextField txtDiscount;
+    @FXML
+    private TextField txtComment;
+    @FXML
+    private Label lbInvoiceStatus;
+    @FXML
+    private TextField txtInvoiceIdSearch;
+    @FXML
+    private ComboBox<?> cbCustomerSearch;
+    @FXML
+    private ComboBox<?> cbInvoiceTypeSearch;
+    @FXML
+    private Button resetInvoicesListTable;
+    @FXML
+    private TableView<?> tbInvoicesList;
+    @FXML
+    private TableView<?> tbProducts;
 
     /**
      * Initializes the controller class.
@@ -23,5 +83,61 @@ public class SalesInvoicesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void saveCustomer(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateCustomer(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteCustomer(ActionEvent event) {
+    }
+
+    @FXML
+    private void filterCustomerTable(KeyEvent event) {
+    }
+
+    @FXML
+    private void openFileChooser(ActionEvent event) {
+    }
+
+    @FXML
+    private void addProductToTable(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteProduct(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteAllProducts(ActionEvent event) {
+    }
+
+    @FXML
+    private void saveInvoice(ActionEvent event) {
+    }
+
+    @FXML
+    private void printInvoice(ActionEvent event) {
+    }
+
+    @FXML
+    private void newInvoice(ActionEvent event) {
+    }
+
+    @FXML
+    private void viewInvoice(ActionEvent event) {
+    }
+
+    @FXML
+    private void updateInvoice(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteInvoice(ActionEvent event) {
+    }
     
 }
